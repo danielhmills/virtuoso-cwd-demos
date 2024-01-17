03f5810df8cf3afc1b29dbe1bf6fbf6c80a9de15
danielhmills <daniel.h.mills@gmx.de>
2024-01-17 17:13:14 -0500

    Added setup .SQL for attaching CSVs to tables

---
+++ wine/setup.sql
@@ -0,0 +1,3 @@
+ATTACH_FROM_CSV('spider.wine.grapes','wine_1/data_csv/grapes.csv',',','\n',null,1,vector(1));
+ATTACH_FROM_CSV('spider.wine.appellations','wine_1/data_csv/appellations.csv',',','\n',null,1,vector(1));
+ATTACH_FROM_CSV('spider.wine.wine','wine_1/data_csv/wine.csv',',','\n',null,1,null);
