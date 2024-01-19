3873b7c6b559b4b0cecd827defee95ebdc79eee8
danielhmills <daniel.h.mills@gmx.de>
2024-01-19 02:43:40 -0500

    attach csvs to tables

--- wine/setup.sql
+++ wine/setup.sql
@@ -1,3 +1,7 @@
-ATTACH_FROM_CSV('spider.wine.grapes','wine_1/data_csv/grapes.csv',',','\n',null,1,vector(1));
-ATTACH_FROM_CSV('spider.wine.appellations','wine_1/data_csv/appellations.csv',',','\n',null,1,vector(1));
-ATTACH_FROM_CSV('spider.wine.wine','wine_1/data_csv/wine.csv',',','\n',null,1,null);
+ATTACH_FROM_CSV('spider.wine.grapes','virtuoso-cwd-demos/wine/data/grapes.csv',',','\n',null,1,vector(1));
+ATTACH_FROM_CSV('spider.wine.appellations','virtuoso-cwd-demos/wine/data/appellations.csv',',','\n',null,1,vector(1));
+ATTACH_FROM_CSV('spider.wine.wine','virtuoso-cwd-demos/wine/data/wine.csv',',','\n',null,1,null);
+
+GRANT SELECT ON spider.wine.grapes TO SPARQL_SELECT;
+GRANT SELECT ON spider.wine.appellations TO SPARQL_SELECT;
+GRANT SELECT ON spider.wine.wine TO SPARQL_SELECT;
